feaff12f7c247b2e0ed15ed8879f97f503c1c17d
thandazilegcisa <gcisatahn@gmail.com>
2021-09-29 01:29:37 +0200

    Blogs

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ promise.then(function(resolve){
     
 })
 */
+/*
 const main = document.getElementById("main");
 main.classList.add("Hello");
 const button = document.createElement("button");
@@ -76,3 +77,32 @@ window.addEventListener("click", function(event){
     console.log(`X-Axis: ${xAxis} Y-Axis: ${yAxis}`);
 
 });
+*/
+const uiNavElements = [
+    {title: "Home", link: "../index.html"},
+    {title: "Blogs", link: "../Blogs/blogIndexPage.html"},
+    {title: "Data", link: "../Data/dataIndex.html"},
+    {title: "About", link: "../About/aboutIndexPage.html"}
+]
+
+function displayUiNavElements(){
+    let nav = document.getElementById("navigation");
+    let ul = document.createElement("ul");
+    ul.setAttribute("id", "listItem-Container");
+    nav.appendChild(ul);
+
+
+    for(let i = 0; i<uiNavElements.length; i++){
+        const li = document.createElement("li");
+        li.setAttribute("class","list-Item");
+        const a = document.createElement("a");
+
+        a.innerHTML = uiNavElements[i].title;
+        a.href = uiNavElements[i].link;
+
+        ul.appendChild(li);
+        li.appendChild(a);
+
+    }
+}
+document.addEventListener('DOMContentLoaded', displayUiNavElements());
